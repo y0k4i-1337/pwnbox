0fea0c4d76fb87f02c1c9ebe0950e7694c70501b
Mayk <7395296+yok4i@users.noreply.github.com>
2022-12-06 22:00:05 -0300

    Add bash config

---
+++ opt/vpnpanel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+htbip=$(ip addr | grep tun0 | grep inet | grep 10. | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
+lab=$(cat /etc/openvpn/*.conf | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-)
+
+if [[ $htbip == *"10."* ]]
+then
+   gwip=$(route -n | grep tun0 | grep UG | tr -s " " | cut -d " " -f 2 | sort -u)
+   ping=$(ping -c 1 $gwip -W 1 | sed '$!d;s|.*/\([0-9.]*\)/.*|\1|' | cut -c1-4)
+   echo "VPN: ($htbip) [$ping ms]"
+else
+   echo " "
+fi
+
